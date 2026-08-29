88cc65cadfd7010aca853d614674df2645c99995
Rohit R <rlrohit2007@gmail.com>
2026-08-29 17:12:13 +0530

    Fix Aiven MySQL connection timeout

--- config/db.js
+++ config/db.js
@@ -4,9 +4,19 @@ const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
+  connectTimeout: 10000
 });
 
 module.exports = pool;
